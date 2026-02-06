8b29f357bf599b37e07e9d6db99ea1d0be080dbd
ouob123 <kyjin320@sookmyung.ac.kr>
2026-02-07 00:10:29 +0900

    Chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/OneWave/OneWaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.OneWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OneWave/OneWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OneWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
